bb6a7baed86e6a1c9116ac740b86315fa373e8b8
u_dev <uddhavdave1@gmail.com>
2021-10-11 21:24:03 +0530

    [0.1.0] Basic smart contract [1/2] MVP

--- programs/prize_pool_contract/src/lib.rs
+++ programs/prize_pool_contract/src/lib.rs
@@ -1,61 +1,162 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::account_info::Account;
-use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer};
-use check_creator::check_creator;
-use check_creator::cpi::accounts::{CancelCheck, CashCheck, CreateEmptyCheck};
-use check_creator::program::check_creator;
-use check_creator::{self, Check};
+use anchor_spl::token::{self, TokenAccount, Transfer};
+use std::convert::Into;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GDtbF64JLEbuoGxRTrVob8wef28sByS8GZn4JzSBJTCW");
 
 #[program]
 pub mod prize_pool_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializePlayerVault>, amount:u64, nonce:u8) -> ProgramResult {
+    pub fn initialize(ctx: Context<InitializeMatch>) -> ProgramResult {
         let game = &mut ctx.accounts.game;
         game.match_address = *game.to_account_info().key;
-        game.player_1_token_account = *ctx.accounts.player_x.key;
         Ok(())
-        //Create Empty Check for the first player
-        let cpi_program = ctx.accounts.match_manager.to_account_info();
-        let cpi_accounts = ctx.check;
+    }
+
+    pub fn register_player_one(
+        ctx: Context<InitializePlayerVault>,
+        amount: u64,
+        nonce: u8,
+    ) -> ProgramResult {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info().clone(),
+            to: ctx.accounts.player_1_vault.to_account_info().clone(),
+            authority: ctx.accounts.owner.clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.clone();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        check_creator::cpi::create_empty_check(cpi_ctx, amount, nonce);
+        token::transfer(cpi_ctx, amount)?;
+
+        let game = &mut ctx.accounts.game;
+        game.player_1_token_account = *ctx.accounts.player_1_vault.to_account_info().key;
+        game.wager_amount += amount;
+
+        // Print the check.
+        let check = &mut ctx.accounts.player_1_check;
+        check.amount = amount;
+        check.from = *ctx.accounts.from.to_account_info().key;
+        check.vault = *ctx.accounts.player_1_vault.to_account_info().key;
+        check.nonce = nonce;
+        Ok(())
+    }
+
+    pub fn register_player_two(ctx: Context<StartMatch>, amount: u64, nonce: u8) -> ProgramResult {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info().clone(),
+            to: ctx.accounts.player_2_vault.to_account_info().clone(),
+            authority: ctx.accounts.owner.clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+
+        let game = &mut ctx.accounts.game;
+        game.player_2_token_account = *ctx.accounts.player_2_vault.to_account_info().key;
+        game.wager_amount += amount;
+
+        // Print the check.
+        let check = &mut ctx.accounts.player_2_check;
+        check.amount = amount;
+        check.from = *ctx.accounts.from.to_account_info().key;
+        check.vault = *ctx.accounts.player_2_vault.to_account_info().key;
+        check.nonce = nonce;
+        Ok(())
     }
 }
 
-//Adds data for player 2 and starts the match with given seed 
 #[derive(Accounts)]
 pub struct InitializeMatch<'info> {
-    #[account(signer)]
-    player_2_vault: AccountInfo<'info>,
-    #[account(mut, constraint = game.winner_address != Pubkey::default() && game.player_1_token_account != Pubkey::default())]
+    #[account(zero)]
     game: Account<'info, Match>,
+    #[account(signer)]
+    authority: AccountInfo<'info>,
 }
 
 //Gets player 1 data and adds it to match account
 #[derive(Accounts)]
 pub struct InitializePlayerVault<'info> {
-    #[account(zero)]
+    #[account(mut)]
     game: Account<'info, Match>,
     #[account(zero)]
     player_1_check: Account<'info, Check>,
-    //Vault made for storing wager amount
-    #[account(mut, has_one = player_wallet)]
-    player_1_vault: Account<'info, TokenAccount>,,
-    player_1_wallet: AccountInfo<'info>,
-    match_manager: Program<'info, check_creator>,
+    // Check's token vault.
+    #[account(mut, constraint = &player_1_vault.owner == check_signer.key)]
+    player_1_vault: Account<'info, TokenAccount>,
+    // Program derived address for the check.
+    check_signer: AccountInfo<'info>,
+    // Token account the check is made from.
+    #[account(mut, has_one = owner)]
+    from: Account<'info, TokenAccount>,
+    // Owner of the `from` token account.
+    #[account(signer)]
+    owner: AccountInfo<'info>,
+    token_program: AccountInfo<'info>,
 }
-    
 
-#[derive(Accounts)]
-pub struct ConcludeMatch<'info> {
-    game: ProgramAccount<'info, Match>,
-    #[account(mut, has_one = authority)]
-    winner_address: Pubkey,
-    authority: Signer<'info>,
+impl<'info> InitializePlayerVault<'info> {
+    pub fn accounts(ctx: &Context<InitializePlayerVault>, nonce: u8) -> Result<()> {
+        let signer = Pubkey::create_program_address(
+            &[
+                ctx.accounts.player_1_check.to_account_info().key.as_ref(),
+                &[nonce],
+            ],
+            ctx.program_id,
+        )
+        .map_err(|_| ErrorCode::InvalidCheckNonce)?;
+        if &signer != ctx.accounts.check_signer.to_account_info().key {
+            return Err(ErrorCode::InvalidCheckSigner.into());
+        }
+        Ok(())
+    }
 }
+
+//Adds data for player 2 and starts the match with given seed
+#[derive(Accounts)]
+pub struct StartMatch<'info> {
+    #[account(zero)]
+    player_2_check: Account<'info, Check>,
+    #[account(mut, constraint = &player_2_vault.owner == check_signer.key)]
+    player_2_vault: Account<'info, TokenAccount>,
+    // Program derived address for the check.
+    check_signer: AccountInfo<'info>,
+    #[account(mut, has_one = owner)]
+    from: Account<'info, TokenAccount>,
+    // Owner of the `from` token account.
+    #[account(signer)]
+    owner: AccountInfo<'info>,
+    #[account(mut, constraint = game.winner_address == Pubkey::default() && game.player_1_token_account != Pubkey::default())]
+    game: Account<'info, Match>,
+    token_program: AccountInfo<'info>,
+}
+
+impl<'info> StartMatch<'info> {
+    pub fn accounts(ctx: &Context<StartMatch>, amount: u64, nonce: u8) -> Result<()> {
+        let signer = Pubkey::create_program_address(
+            &[
+                ctx.accounts.player_2_check.to_account_info().key.as_ref(),
+                &[nonce],
+            ],
+            ctx.program_id,
+        )
+        .map_err(|_| ErrorCode::InvalidCheckNonce)?;
+        if &signer != ctx.accounts.check_signer.to_account_info().key {
+            return Err(ErrorCode::InvalidCheckSigner.into());
+        }
+        //Wager Amount should be same as player 2
+        if ctx.accounts.game.wager_amount != amount {
+            return Err(ErrorCode::InvalidWager.into());
+        }
+        Ok(())
+    }
+}
+// #[derive(Accounts)]
+// pub struct ConcludeMatch<'info> {
+//     game: Account<'info, Match>,
+//     #[account(mut, has_one = authority)]
+//     winner_address: Account<'info>,
+//     authority: Signer<'info>,
+// }
 
 #[account]
 #[derive(Default)]
@@ -66,4 +167,29 @@ pub struct Match {
     prize_settled: bool,
     match_address: Pubkey,
     winner_address: Pubkey,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Check {
+    from: Pubkey,
+    to: Pubkey,
+    amount: u64,
+    vault: Pubkey,
+    nonce: u8,
+    burned: bool,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("The given nonce does not create a valid program derived address.")]
+    InvalidCheckNonce,
+    #[msg("The derived check signer does not match that which was given.")]
+    InvalidCheckSigner,
+    #[msg("The given check has already been burned.")]
+    AlreadyBurned,
+    #[msg("Unauthorized Claim Request")]
+    InvalidAuthClaim,
+    #[msg("Incompatible Wager")]
+    InvalidWager,
 }
--- tests/prize_pool_contract.js
+++ tests/prize_pool_contract.js
@@ -1,14 +1,156 @@
 const anchor = require('@project-serum/anchor');
+const serumCmn = require("@project-serum/common");
+const assert = require("assert");
+const { TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 
 describe('prize_pool_contract', () => {
-
+  
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.PrizePoolContract;
+  const check1 = anchor.web3.Keypair.generate();
+  const check2 = anchor.web3.Keypair.generate();
+  const vault1 = anchor.web3.Keypair.generate();
+  const vault2 = anchor.web3.Keypair.generate();
+  const match = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
+  let mint = null;
+  let god = null;
+
+  it("Sets up initial test state", async () => {
+    const [_mint, _god] = await serumCmn.createMintAndVault(
+      program.provider,
+      new anchor.BN(1000000)
+    );
+    mint = _mint;
+    god = _god;
+  });
+
+  let check1Signer = null;
+  let check2Signer = null;
+
+  it('Initialize Match', async () => {
+    const tx = await program.rpc.initialize({
+      accounts: {
+        authority: program.provider.wallet.publicKey,
+        game: match.publicKey,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [match],
+      instructions: [await program.account.match.createInstruction(match)]
+    })
+
+    console.log("transaction: ", tx)
+  });
+
+  it('Ready Player one!', async () => {
     // Add your test here.
-    const program = anchor.workspace.PrizePoolContract;
-    const tx = await program.rpc.initialize();
+    let [_checkSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [check1.publicKey.toBuffer()],
+      program.programId
+    );
+    check1Signer = _checkSigner;
+    
+    const tx = await program.rpc.registerPlayerOne(
+      new anchor.BN(1000),
+      nonce,
+      {
+        accounts: {
+          game: match.publicKey,
+          player1Check: check1.publicKey,
+          player1Vault: vault1.publicKey,
+          checkSigner: check1Signer,
+          from: god,
+          owner: program.provider.wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+        signers: [check1, vault1],
+        instructions: [
+          await program.account.check.createInstruction(check1, 300),
+          ...(await serumCmn.createTokenAccountInstrs(
+            program.provider,
+            vault1.publicKey,
+            mint,
+            check1Signer
+          )),
+        ],
+      }
+    );
+
+    const checkAccount = await program.account.check.fetch(check1.publicKey);
+    assert.ok(checkAccount.from.equals(god));
+    assert.ok(checkAccount.amount.eq(new anchor.BN(1000)));
+    assert.ok(checkAccount.vault.equals(vault1.publicKey));
+    assert.ok(checkAccount.nonce === nonce);
+    assert.ok(checkAccount.burned === false);
+
+    let vaultAccount = await serumCmn.getTokenAccount(
+      program.provider,
+      checkAccount.vault
+    );
+    assert.ok(vaultAccount.amount.eq(new anchor.BN(1000)));
+
     console.log("Your transaction signature", tx);
+  });
+
+  it('Ready Player two and start match!', async () => {
+    // Add your test here.
+    let [_checkSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [check2.publicKey.toBuffer()],
+      program.programId
+    );
+    check2Signer = _checkSigner;
+    
+    const tx = await program.rpc.registerPlayerTwo(
+      new anchor.BN(1000),
+      nonce,
+      {
+        accounts: {
+          game: match.publicKey,
+          player2Check: check2.publicKey,
+          player2Vault: vault2.publicKey,
+          checkSigner: check2Signer,
+          from: god,
+          owner: program.provider.wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+        signers: [check2, vault2],
+        instructions: [
+          await program.account.check.createInstruction(check2, 300),
+          ...(await serumCmn.createTokenAccountInstrs(
+            program.provider,
+            vault2.publicKey,
+            mint,
+            check2Signer
+          )),
+        ],
+      }
+    );
+    
+    const checkAccount = await program.account.check.fetch(check2.publicKey);
+    assert.ok(checkAccount.from.equals(god));
+    assert.ok(checkAccount.amount.eq(new anchor.BN(1000)));
+    assert.ok(checkAccount.vault.equals(vault2.publicKey));
+    assert.ok(checkAccount.nonce === nonce);
+    assert.ok(checkAccount.burned === false);
+    
+    let vaultAccount = await serumCmn.getTokenAccount(
+      program.provider,
+      checkAccount.vault
+    );
+    assert.ok(vaultAccount.amount.eq(new anchor.BN(1000)));
+    console.log("Your transaction signature", tx);
+    
+    
+    const gameData = await program.account.match.fetch(match.publicKey);
+    assert.ok(gameData.player1TokenAccount.equals(vault1.publicKey));
+    assert.ok(gameData.player2TokenAccount.equals(vault2.publicKey));
+    assert.ok(gameData.matchAddress.equals(match.publicKey));
+    assert.ok(gameData.wagerAmount.eq(new anchor.BN(2000)));
+    assert.ok(gameData.prizeSettled == false);
+    console.log("Match is set successfully, on Address ", gameData.matchAddress);
+      
   });
 });
